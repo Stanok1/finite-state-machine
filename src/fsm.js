bd8dd8d6209d0e36f2a2efb6ddd33ac963b4cd5f
Stanok1 <konats96@gmail.com>
2017-02-04 21:59:10 +0300

    finite-state-machine:final

--- src/fsm.js
+++ src/fsm.js
@@ -1,79 +1,35 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
     constructor(config) {
         this.config = config;
         this.state = config.initial;
         this.hist = [];
-        this.hist.push(this.state);
-        this.chist=[];
+        this.chist = [];
     }
-
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
     getState() {
         return this.state;
     }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
     changeState(state) {
-
-
-   if (state in this.config.states){
-    this.hist.push(this.state);
-    this.state = state;}else {
-
-
-
-    throw new Error;
-}
-
-}
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-     trigger(event) {
-       if (this.config.states[this.state].transitions[event] != undefined)
-       {
-        this.chist.push(event);
-         this.hist.push(this.state);
-         this.state = this.config.states[this.state].transitions[event];
-       }else {
-         throw new Error;
-       }
-     }
-
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {
-
-        this.state = this.config.initial;
-
+        if (state in this.config.states) {
+            this.hist.push(this.state);
+            this.state = state;
+        } else {
+            throw new Error;
+        }
     }
-
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
+    trigger(event) {
+        if (this.config.states[this.state].transitions[event] != undefined) {
+            this.chist.push(event);
+            this.hist.push(this.state);
+            this.state = this.config.states[this.state].transitions[event];
+        } else {
+            throw new Error;
+        }
+    }
+    reset() {
+        this.state = this.config.initial;
+    }
     getStates(event) {
-        /*
-(anonymous) @ VM2335:1
-'busy' in student.config.states
-true
-Object.keys(student.config.states)
-
-*/
         this.getSt = [];
         this.cstat = [];
         this.cstat = Object.keys(this.config.states);
@@ -93,51 +49,26 @@ Object.keys(student.config.states)
         }
         return this.getSt;
     }
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
     undo() {
-
-        if (this.hist.length == 1) {
+        if (this.hist.length == 0) {
+            return false;
+        } else {
+            this.state = this.hist.pop();
+            return true;
+        }
+    }
+    redo() {
+        if (this.chist.length == 0 || this.config.states[this.state].transitions[this.chist[0]] == undefined) {
             return false;
 
         } else {
-           this.state = this.hist.pop();
-           return true;
-
-
+            this.state = this.config.states[this.state].transitions[this.chist.shift()];
+            return true;
         }
-
-
     }
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {
-      if (this.chist.length == 0) {
-          return false;
-
-      } else {
-         this.trigger(this.chist.pop());
-         this.hist.pop();
-
-      }
-
-
-    }
-
-    /**
-     * Clears transition history
-     */
     clearHistory() {
-      this.hist = [];
-      this.chist = [];
-
+        this.hist = [];
+        this.chist = [];
     }
 }
 module.exports = FSM;
